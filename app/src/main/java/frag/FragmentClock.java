c3de6ac6dee34e0c79c2771616f16b92018602f0
lazor <2638903554@qq.com>
2019-06-25 15:14:40 +0800

    闹钟界面的一些布局文件

--- app/src/main/java/frag/FragmentClock.java
+++ app/src/main/java/frag/FragmentClock.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.tranquil.MusicListActivity;
@@ -18,24 +19,15 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 
 public class FragmentClock extends Fragment implements View.OnClickListener {
 
-    @ViewInject(R.id.clock1)
-    public TextView clock1;
-    @ViewInject(R.id.clock2)
-    public TextView clock2;
-    @ViewInject(R.id.newClock)
-    public TextView newClock;
+    public ImageView clockadd;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=LayoutInflater.from(getActivity()).inflate(R.layout.frag_clock,null);
         ViewUtils.inject(getActivity());
-        clock1= view.findViewById(R.id.clock1);
-        clock1.setOnClickListener(this);
-        clock2 = view.findViewById(R.id.clock2);
-        clock2.setOnClickListener(this);
-        newClock = view.findViewById(R.id.newClock);
-        newClock.setOnClickListener(this);
+        clockadd=view.findViewById(R.id.clockadd);
+        clockadd.setOnClickListener(this);
         return view;
     }
 
@@ -50,13 +42,7 @@ public class FragmentClock extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.clock1://跳到闹钟1编辑界面
-                startActivity(new Intent(getActivity(), SetAlarmActivity.class));
-                break;
-            case R.id.clock2://跳到闹钟2编辑界面
-                startActivity(new Intent(getActivity(), SetAlarmActivity.class));
-                break;
-            case R.id.newClock:// 新建界面
+            case R.id.clockadd:// 新建界面
                 startActivity(new Intent(getActivity(),SetAlarmActivity.class));
                 break;
         }
